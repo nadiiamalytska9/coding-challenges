b7dd19f44bf77d3dd53890e625afa2a209d9f8d1
nadia.malytska <nadiia.malytska@leovegas.com>
2020-09-01 09:47:18 +0300

    Add LRU cache implementation based on LinkedHashMap, add tests forLruCache.

---
+++ src/main/java/solutions/LruCache.java
@@ -0,0 +1,22 @@
+package solutions;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class LruCache<K, V> extends LinkedHashMap<K, V> {
+    private int MAX_SIZE;
+
+    private LruCache(int size) {
+        super(size, 0.75f, true);
+        this.MAX_SIZE = size;
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > MAX_SIZE;
+    }
+
+    public static <K, V> LruCache<K, V> newInstant(int size) {
+        return new LruCache<>(size);
+    }
+}
